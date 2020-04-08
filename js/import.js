35e135b1af0c8a47e6fec1890ddd48271df781aa
seerafe <seerafe@sina.com>
2020-04-08 09:57:52 +0800

    常规更新

--- js/import.js
+++ js/import.js
@@ -6,6 +6,10 @@ app.import=function(src){
 };
 RUI_CSS= "<link href=\"bin/pro.rui.code/get/pro.rui.ant_min-M.css\" rel=\"stylesheet\">";
 RUI_JQUERY= "<script src=\"bin/pro.rui.code/get/com.jquery_min.js\"></script>";
-RUI_CORE= "<script data-imports=\"icon,table\"  src=\"bin/pro.rui.code/get/pro.rui.ant_min.js\"></script>";
+if(location.href.indexOf(".loc") != -1){
+    RUI_CORE= "<script  data-imports=\"icon,table\"  src=\"bin/pro.rui.code/get/pro.rui.ant_min.js\"></script>";
+ 
+}else
+RUI_CORE= "<script data-root=\"/rui-design/bin/pro.rui.lib/\" data-imports=\"icon,table\"  src=\"bin/pro.rui.code/get/pro.rui.ant_min.js\"></script>";
  
  
